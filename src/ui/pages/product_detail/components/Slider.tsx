ea34fd412af707b28f5aa964aabf8175a18a5632
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-11 22:48:30 +0200

    productos relacionados y custom scroll

---
+++ src/ui/pages/product_detail/components/Slider.tsx
@@ -0,0 +1,24 @@
+import './Slider.modules.css'
+import { useScroll } from '../../../../hooks/useScroll'
+
+export const Slider = ({ children }: { children: React.ReactNode }) => {
+  const { containerRef, scrollPercentage, scrollWidth, clientWidth } =
+    useScroll()
+
+  return (
+    <div className='slider__container'>
+      <section className='slider' ref={containerRef}>
+        <div className='slider__content'>{children}</div>
+      </section>
+      <div className='slider__scroll'>
+        <div
+          style={{
+            width: `${(clientWidth * 100) / scrollWidth}%`,
+            transform: `translateX(${scrollPercentage}%)`,
+          }}
+          className='slider__scroll-bar'
+        ></div>
+      </div>
+    </div>
+  )
+}
