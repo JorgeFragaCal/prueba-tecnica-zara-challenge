5b2f237a4587326fa653947aaad974cca11b000d
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-11 17:09:24 +0200

    searchBar component

--- src/ui/components/search_bar/SearchBar.tsx
+++ src/ui/components/search_bar/SearchBar.tsx
@@ -1,3 +1,24 @@
-export const SearchBar = () => {
-  return <div>SearchBar</div>
+import './SearchBar.modules.css'
+export const SearchBar = ({
+  placeholder = 'Search for a smartphone...',
+  results = 0,
+  value = '',
+  onChange = () => {},
+}: {
+  placeholder?: string
+  results?: number
+  value?: string
+  onChange?: (value: string) => void
+}) => {
+  return (
+    <div className='search-bar'>
+      <input
+        type='text'
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <span>{results} results</span>
+    </div>
+  )
 }
