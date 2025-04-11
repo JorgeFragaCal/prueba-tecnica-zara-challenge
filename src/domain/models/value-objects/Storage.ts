6eacc1dddd44fcd7cb9d5fb8d964f9d8851653dd
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-11 17:45:20 +0200

    enrutado y pagina detalle

--- src/domain/models/value-objects/Storage.ts
+++ src/domain/models/value-objects/Storage.ts
@@ -4,18 +4,10 @@ export class Storage {
   private constructor(
     private readonly capacity: string,
     private readonly price: Price,
-  ) {
-    if (!this.isValidCapacity(capacity)) {
-      throw new Error('Capacidad inválida')
-    }
-  }
+  ) {}
 
   static create(capacity: string, price: Price): Storage {
     return new Storage(capacity, price)
-  }
-
-  private isValidCapacity(capacity: string): boolean {
-    return /^\d+(?:GB|TB)$/.test(capacity)
   }
 
   getCapacity(): string {
