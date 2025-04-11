406ff6394029ea328d34d2982aada9b0a3740e1f
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-11 22:48:47 +0200

    seccion de especificaciones

--- src/ui/pages/product_detail/ProductDetailPage.tsx
+++ src/ui/pages/product_detail/ProductDetailPage.tsx
@@ -3,7 +3,8 @@ import { getSmartphoneById } from '@/application/usecases'
 import { Product } from '@/domain/models/interfaces'
 import { LoadingBar, SmartphoneCard } from '@/ui/components'
 import { deleteDuplicate } from '@/utils/deleteDuplicate'
-import { ProductInfoHeader } from './components/ProductInfoHeader'
+import { ProductInfoHeader, Specifications } from './components'
+import { Slider } from './components/Slider'
 export const ProductDetailPage = ({ id }: { id: string }) => {
   const {
     data: product,
@@ -14,50 +15,13 @@ export const ProductDetailPage = ({ id }: { id: string }) => {
   if (loading) return <LoadingBar loading={loading} />
   if (error) return <div>Error: {error.message}</div>
   if (!product) return <div>Producto no encontrado</div>
-
   return (
     <main className='product-detail container'>
       <ProductInfoHeader product={product} />
 
-      <section className='product-detail__specs'>
-        <h2>Especificaciones técnicas</h2>
-        <div className='specs-grid'>
-          <div className='spec-item'>
-            <span>Pantalla</span>
-            <span>{product.specs.screen}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Resolución</span>
-            <span>{product.specs.resolution}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Procesador</span>
-            <span>{product.specs.processor}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Cámara principal</span>
-            <span>{product.specs.mainCamera}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Cámara frontal</span>
-            <span>{product.specs.selfieCamera}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Batería</span>
-            <span>{product.specs.battery}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Sistema operativo</span>
-            <span>{product.specs.os}</span>
-          </div>
-          <div className='spec-item'>
-            <span>Frecuencia de actualización</span>
-            <span>{product.specs.screenRefreshRate}</span>
-          </div>
-        </div>
-      </section>
+      <Specifications product={product} />
 
-      <section className='slider'>
+      <Slider>
         {product.similarProducts.length > 0 &&
           deleteDuplicate(product.similarProducts).map((product) => (
             <SmartphoneCard
@@ -66,7 +30,7 @@ export const ProductDetailPage = ({ id }: { id: string }) => {
               key={product.id}
             />
           ))}
-      </section>
+      </Slider>
     </main>
   )
 }
