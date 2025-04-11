531f12986463e685ad9d525cb0190a0c24759ac2
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-11 10:54:21 +0200

    añadir metodo fetch de listado productos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { ProductList } from './domain/models/ProductList'
+import { useFetch } from './hooks/useFetch'
+
 function App() {
-  return <>Pagina de inicio</>
+  const { data, loading, error } = useFetch<ProductList[] | null>(
+    'https://prueba-tecnica-api-tienda-moviles.onrender.com/products',
+  )
+
+  if (loading) return <div>Cargando...</div>
+  if (error) return <div>Error: {error.message}</div>
+
+  return (
+    <div>
+      {data
+        ?.filter(
+          (product, index, self) =>
+            index === self.findIndex((p) => p.id === product.id),
+        )
+        .map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.brand}</p>
+            <p>{product.basePrice}</p>
+            <p>{product.imageUrl}</p>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default App
