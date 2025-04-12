ad738f3a6932b45b6011d6bd6586d9b1a59995b8
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-12 04:04:51 +0200

    fix datos faltantes card cart

--- src/ui/components/product_card/ProductCardCart.tsx
+++ src/ui/components/product_card/ProductCardCart.tsx
@@ -1,30 +1,32 @@
-import { ProductList } from '@/domain/models/ProductList'
 import './ProductCardCart.modules.css'
 import { useCart } from '@/context/CartContext'
+import { Product } from '@/domain/models/interfaces'
 export const ProductCardCart = ({
   product,
 }: {
-  product: Omit<ProductList, 'brand'> & { quantity?: number }
+  product: Product & { quantity?: number }
 }) => {
   const { removeItemFromCart } = useCart()
   return (
     <div className='product-card-cart' key={product.id}>
-      <img src={product.imageUrl} alt={product.name} width={100} height={180} />
+      <img
+        src={product.colorOptions[0].getImageUrl()}
+        alt={product.name}
+        width={100}
+        height={180}
+      />
       <div className='product-card-cart__info'>
         <div>
-          <h2>{product.name}</h2>
+          <div>
+            <h2>{product.name}</h2>
+            <p>{product.colorOptions[0].getName()}</p>
+            <p>{product.storageOptions[0].getCapacity()}</p>
+          </div>
           <p>{product.basePrice.toString()}</p>
         </div>
         <button
           aria-label='Remove product'
-          onClick={() =>
-            removeItemFromCart({
-              id: product.id,
-              basePrice: product.basePrice,
-              imageUrl: product.imageUrl,
-              name: product.name,
-            })
-          }
+          onClick={() => removeItemFromCart(product)}
         >
           Remove ({product.quantity})
         </button>
