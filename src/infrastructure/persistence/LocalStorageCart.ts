230492bcf3f9c3b83a8f333eafdae09357045de6
Jorge Fraga <jorge.fraga1997@gmail.com>
2025-04-12 02:12:20 +0200

    manejar la duplicidad de productos iguales en el carrito

--- src/infrastructure/persistence/LocalStorageCart.ts
+++ src/infrastructure/persistence/LocalStorageCart.ts
@@ -1,18 +1,45 @@
+import { ProductBase } from '@/domain/models/interfaces'
+import { Price } from '@/domain/models/value-objects'
+
 const CART_KEY = 'cart'
 
-export const getCartItems = (): string[] => {
+export const getCartItems = (): (Omit<ProductBase, 'brand'> & {
+  quantity?: number
+})[] => {
   const data = localStorage.getItem(CART_KEY)
-  return data ? JSON.parse(data) : []
+  if (!data) return []
+
+  const parsedItems = JSON.parse(data)
+  return parsedItems.map((item: any) => ({
+    ...item,
+    basePrice: Price.create(item.basePrice.amount, item.basePrice.currency),
+  }))
 }
 
-export const addItemToLocalStorageCart = (id: string): void => {
+export const addItemToLocalStorageCart = (
+  item: Omit<ProductBase, 'brand'> & { quantity?: number },
+): void => {
   const items = getCartItems()
-  items.push(id)
+  const itemIndex = items.findIndex(
+    (i) =>
+      i.id === item.id &&
+      i.basePrice.getValue() === item.basePrice.getValue() &&
+      i.imageUrl === item.imageUrl,
+  )
+
+  if (itemIndex !== -1) {
+    items[itemIndex].quantity = (items[itemIndex].quantity ?? 1) + 1
+  } else {
+    items.push({ ...item, quantity: 1 })
+  }
+
   localStorage.setItem(CART_KEY, JSON.stringify(items))
 }
 
-export const removeItemFromLocalStorageCart = (id: string): void => {
-  const items = getCartItems().filter((i) => i !== id)
+export const removeItemFromLocalStorageCart = (
+  item: Omit<ProductBase, 'brand'>,
+): void => {
+  const items = getCartItems().filter((i) => i !== item)
   localStorage.setItem(CART_KEY, JSON.stringify(items))
 }
 
